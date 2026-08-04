9d0ce9003ecc9dd40cb4398687000897ffc52070
Hashir Ahmed <hashir.corporateservices@gmail.com>
2026-08-04 16:21:24 +0400

    Add files via upload

---
+++ backend/app/Providers/AppServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        // Microsoft isn't a core Socialite driver; register it from the community package.
+        Event::listen(function (SocialiteWasCalled $event) {
+            $event->extendSocialite('microsoft', \SocialiteProviders\Microsoft\Provider::class);
+        });
+    }
+}
